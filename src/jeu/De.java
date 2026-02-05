d1214edc8e3ed43778c12c090d360ea9fd1489da
antonin-grn <antonin.gerain@gmail.com>
2026-02-05 11:59:22 +0100

    Fin scéance 1

--- src/jeu/De.java
+++ src/jeu/De.java
@@ -1,8 +1,23 @@
 package jeu;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class De {
-	private int valeur;
+	private int valeurMax;
 	private Random random;
 	
+	public De (int valeurMax) {
+		this.valeurMax = valeurMax;
+	}
+	
+	public int lancerDe() {
+		try {
+			random = SecureRandom.getInstanceStrong();
+			} catch (Exception e) {
+			e.printStackTrace();
+			}
+		return random.nextInt(1, valeurMax + 1);
+	}
 	
 }
